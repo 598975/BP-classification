fe1d3fb9d4c1243ce1e1bfcb1c892b13583b394e
598975 <89256888+598975@users.noreply.github.com>
2025-12-08 14:54:34 +0100

    clustering with embeddings testing

--- util/structural_diff.py
+++ util/structural_diff.py
@@ -14,7 +14,7 @@ def normalize_blueprint(obj):
         return normalize_text(str(obj))
 
 
-def load_and_normalize_from_topic_id(topic_id=None, bps=None):
+def load_and_normalize_blueprints(topic_id=None, bps=None):
     if bps:
         return [normalize_blueprint(parse_yaml(bp.blueprint_code)) for bp in bps]
     db = Database()
@@ -35,8 +35,17 @@ def structural_diff(code1, code2):
     return diff, 1 - diff_size / total_size
 
 
-def compare_multiple_bps(bps: list[Blueprint]) -> list[list]:
-    normalized_codes = load_and_normalize_from_topic_id(bps=bps)
+def compare_multiple_bps(bps: list[Blueprint]) -> list[tuple[Blueprint, Blueprint, float]]:
+    """
+    Compare multiple blueprints and return their pairwise structural differences.
+    
+    :param bps: List of Blueprint objects to compare.
+    :type bps: list[Blueprint]
+    :return: List of tuples containing pairs of Blueprints and their similarity score.
+    :rtype: list[tuple[Blueprint, Blueprint, float]]
+    """
+    
+    normalized_codes = load_and_normalize_blueprints(bps=bps)
     comparison = []
     for i in range(len(normalized_codes)):
         for j in range(i + 1, len(normalized_codes)):
